ab98f18bad90d7e378e137fb5f3405ae52ee7189
linnguyen <nguyenvanlin01041994@gmail.com>
2019-01-14 10:49:59 +0700

    1. Add contact us to header 2. Remove override contact us

--- config/initializers/spree_contact_us.rb
+++ config/initializers/spree_contact_us.rb
@@ -15,9 +15,9 @@ SpreeContactUs.setup do |config|
   # ==> Form Configuration
 
   # Configure the form to ask for the users name.
-  config.require_name = false
+  config.require_name = true
 
   # Configure the form to ask for a subject.
-  config.require_subject = false
+  config.require_subject = true
 
 end
