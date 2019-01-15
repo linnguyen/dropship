ebfe519a95dc858c6989875a22c589182c4121c7
linnguyen <nguyenvanlin01041994@gmail.com>
2019-01-15 13:17:22 +0700

    Update mailer config

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -19,7 +19,7 @@ Rails.application.configure do
 
     config.cache_store = :memory_store
     config.public_file_server.headers = {
-      'Cache-Control' => "public, max-age=#{2.days.to_i}"
+        'Cache-Control' => "public, max-age=#{2.days.to_i}"
     }
   else
     config.action_controller.perform_caching = false
@@ -58,4 +58,13 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   # config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+  #
+  # Config mailer
+  config.action_mailer.perform_caching = false
+
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default_url_options = {:host => "157.230.60.184"}
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.smtp_settings = {:address => 'smtp.sendgrid.net', :port => '587'}
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -70,10 +70,10 @@ Rails.application.configure do
   config.action_mailer.perform_caching = false
 
   config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.default_url_options = { :host => ‘sitename.com’ }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default_url_options = {:host => "157.230.60.184"}
   config.action_mailer.perform_deliveries = true
-  ActionMailer::Base.smtp_settings = { :address => ‘smtp.sendgrid.net’, :port => ‘587’, :authentication => :plain, :user_name => ENV[‘SENDGRID_USERNAME’], :password => ENV[‘SENDGRID_PASSWORD’], :domain => ‘heroku.com’ }
-  ActionMailer::Base.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {:address => 'smtp.sendgrid.net', :port => '587'}
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
