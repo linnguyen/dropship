9f3f23e1e97e5f33769edc4fef0c68e15958b763
linnguyen <nguyenvanlin01041994@gmail.com>
2019-01-15 13:25:08 +0700

    Add mailer config production

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,13 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+ActionMailer::Base.smtp_settings = {
+    :address              => "smtp.sendgrid.net",
+    :domain               => DOMAIN,
+    :user_name            => ENV['SENDGRID_USERNAME'],
+    :password             => ENV['SENDGRID_PASSWORD'],
+    :authentication       => "plain",
+    :enable_starttls_auto => true
+}
+ActionMailer::Base.default_url_options = { host: '157.230.60.184' }
