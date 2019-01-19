02d1fb6b0f1671e4052d9f308cf3574b9862467b
lin <nguyenvanlin01041994@gmail.com>
2019-01-19 17:12:46 +0700

    Remove config line in file production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -34,8 +34,6 @@ Rails.application.configure do
 
   config.serve_static_assets = true
 
-  config.assets.precompile =  ['*.js', '*.css', '*.css.erb']
-
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.action_controller.asset_host = 'http://assets.example.com'
 
