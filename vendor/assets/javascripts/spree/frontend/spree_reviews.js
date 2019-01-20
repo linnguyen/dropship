f5ab24c27d9e5d4a7c4474c791f581dd7232344c
lin <nguyenvanlin01041994@gmail.com>
2019-01-20 16:04:45 +0700

    Change style css review-gem to html.erb file

--- vendor/assets/javascripts/spree/frontend/spree_reviews.js
+++ vendor/assets/javascripts/spree/frontend/spree_reviews.js
@@ -2,10 +2,10 @@
 //= require spree/frontend
 //= require spree/frontend/spree_auth
 
-// Navigating to a page with ratings via TurboLinks shows the radio buttons
-$(document).on('page:load', function () {
-    $('input[type=radio].star').rating();
-});
+// // Navigating to a page with ratings via TurboLinks shows the radio buttons
+// $(document).on('page:load', function () {
+//     $('input[type=radio].star').rating();
+// });
 
 // $(document).ready(function () {
 //
