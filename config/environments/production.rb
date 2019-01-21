2eacee6fa0f75c7050a36e2d55049be156e5ad11
lin <nguyenvanlin01041994@gmail.com>
2019-01-21 19:19:09 +0700

    Change  port mailer to 587

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,7 +79,7 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
       :address              => "smtp.gmail.com",
-      :port                 => 465,
+      :port                 => 587,
       :user_name            => 'foo@gmail.com',
       :password             => 'foo',
       :authentication       => :login,
