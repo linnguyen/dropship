972781d0c465c202e5a0d69b22a134084dbbde73
linnguyen <nguyenvanlin01041994@gmail.com>
2019-01-25 16:08:44 +0700

    Add about us and remove name of contact

--- config/initializers/spree_contact_us.rb
+++ config/initializers/spree_contact_us.rb
@@ -15,7 +15,7 @@ SpreeContactUs.setup do |config|
   # ==> Form Configuration
 
   # Configure the form to ask for the users name.
-  config.require_name = true
+  config.require_name = false
 
   # Configure the form to ask for a subject.
   config.require_subject = true
