d50a225eaf9c472ec033127e53d362d721d74455
lin <nguyenvanlin01041994@gmail.com>
2019-02-02 18:10:04 +0700

    Fix page is broken when no image product

--- spree_dolax_sales/app/helpers/spree/products_helper_decorator.rb
+++ spree_dolax_sales/app/helpers/spree/products_helper_decorator.rb
@@ -3,4 +3,10 @@ Spree::ProductsHelper.module_eval do
    def price_before_discount(product)
        (product.price + product.price * 0.45).round(2)
    end
+
+  # This function is randomly for displaying discount percent, for the future will have a field in Database for percent discount, now just
+  # get it from helper class
+  def display_discount
+    [true, false].sample
+  end
 end
